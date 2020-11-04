a00e59f332f2119e2abee0a3058052d7b6aa2eb2
Saketh Kowtha <saketh.kowtha@thoughtworks.com>
2020-11-04 14:36:10 +0530

    Modified load url

--- electron/main.js
+++ electron/main.js
@@ -36,17 +36,17 @@ app.whenReady().then(async () => {
     },
   });
 
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
-  // const url = isDev
-  //   ? "http://localhost:3000"
-  //   : format({
-  //       pathname: join(__dirname, "../renderer/out/index.html"),
-  //       protocol: "file:",
-  //       slashes: true,
-  //     });
+  const url = isDev
+    ? "http://localhost:3000"
+    : format({
+        pathname: join(__dirname, "../renderer/out/index.html"),
+        protocol: "file:",
+        slashes: true,
+      });
 
-  const url = "http://localhost:3000";
+  // const url = "http://localhost:3000";
 
   mainWindow.loadURL(url);
   global.content = getContent();
